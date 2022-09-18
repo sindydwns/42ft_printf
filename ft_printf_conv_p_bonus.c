1dda4e7d6c55e91aa48460fec1234d79e10e3b42
yonshin <yonshin@student.42seoul.kr>
2022-09-18 22:03:04 +0900

    conv p

--- ft_printf_conv_p_bonus.c
+++ ft_printf_conv_p_bonus.c
@@ -6,7 +6,7 @@
 /*   By: yonshin <yonshin@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 12:59:50 by yonshin           #+#    #+#             */
-/*   Updated: 2022/09/14 12:59:50 by yonshin          ###   ########.fr       */
+/*   Updated: 2022/09/18 22:01:48 by yonshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,14 @@
 t_substr	*ft_printf_conv_p(t_parsed_token *token, va_list *valst)
 {
 	void	*value;
-	char	*temp;
-	char	*res;
+	t_strb	sb;
 
-	token++;
 	value = va_arg(*valst, void *);
-	res = ft_tobase((unsigned long)value, "0123456789abcdef");
-	temp = res;
-	res = ft_strjoin("0x", res);
-	free(temp);
-	return (create_substr(res, DETECT_LEN));
+	strb_init(&sb, ft_tobase((unsigned long)value, "0123456789abcdef"), free);
+	sb.add_left(&sb, "0x", 0);
+	if (token->flags & FLAG_DASH)
+		sb.add_right(&sb, ft_strrepeat(" ", token->width - sb.len), free);
+	else
+		sb.add_left(&sb, ft_strrepeat(" ", token->width - sb.len), free);
+	return (create_substr(sb.finish(&sb), DETECT_LEN));
 }

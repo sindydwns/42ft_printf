035c23cd75c8cae90950e5babfbdb3d4424b4e0f
sindydwns <sindydwns@naver.com>
2022-09-20 08:17:18 +0900

    conv x

--- ft_printf_conv_xx_bonus.c
+++ ft_printf_conv_xx_bonus.c
@@ -6,7 +6,7 @@
 /*   By: yonshin <yonshin@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 13:00:09 by yonshin           #+#    #+#             */
-/*   Updated: 2022/09/14 13:00:09 by yonshin          ###   ########.fr       */
+/*   Updated: 2022/09/18 23:39:13 by yonshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,25 @@
 #include <stdarg.h>
 #include "libft.h"
 #include "ft_printf_private_bonus.h"
+#include "advstr.h"
 
 t_substr	*ft_printf_conv_xx(t_parsed_token *token, va_list *valst)
 {
 	unsigned int	value;
-	char			*res;
+	t_strb			sb;
+	char			*padding;
 
-	token++;
 	value = va_arg(*valst, unsigned int);
-	res = ft_tobase(value, "0123456789ABCDEF");
-	return (create_substr(res, DETECT_LEN));
+	if (value == 0 && token->flags & FLAG_DOT)
+		strb_init(&sb, 0, 0);
+	else
+		strb_init(&sb, ft_tobase(value, "0123456789ABCDEF"), free);
+	if (token->flags & FLAG_DOT)
+		sb.add_left(&sb, ft_strrepeat("0", token->precision - sb.len), free);
+	padding = ft_if(token->flags & FLAG_ZERO, "0", " ");
+	if (token->flags & FLAG_DASH)
+		sb.add_right(&sb, ft_strrepeat(padding, token->width - sb.len), free);
+	else
+		sb.add_left(&sb, ft_strrepeat(padding, token->width - sb.len), free);
+	return (create_substr(sb.finish(&sb), DETECT_LEN));
 }

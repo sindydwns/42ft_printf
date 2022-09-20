23e9267f862a5bf9fb88d9522be0be70b68e95b5
yonshin <yonshin@student.42seoul.kr>
2022-09-20 23:55:54 +0900

    rename

--- lib/lstb_clear.c
+++ lib/lstb_clear.c
@@ -6,16 +6,16 @@
 /*   By: yonshin <yonshin@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 03:11:27 by yonshin           #+#    #+#             */
-/*   Updated: 2022/09/13 19:45:20 by yonshin          ###   ########.fr       */
+/*   Updated: 2022/09/20 22:27:51 by yonshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "advlst.h"
 
-t_lstb	*lstb_clear(t_lstb *lstb, t_delf del)
+t_lstb	*lstb_clear(t_lstb *lstb, t_delf content_del)
 {
-	ft_lstclear(&(lstb->list), del);
+	ft_lstclear(&(lstb->list), content_del);
 	lstb_init(lstb, FT_NULL);
 	return (lstb);
 }
